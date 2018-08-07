8d7287420c85aca658044335a82f938ada01b104
Sharon Kynaston <desmogal@gmail.com>
2018-08-07 14:46:10 +0100

    added passengers property and head count method

--- bus_lab.rb
+++ bus_lab.rb
@@ -1,18 +1,22 @@
 
 class Bus
 
-  attr_reader :route, :destination
+  attr_reader :route, :destination, :passengers
 
 
-def initialize(route, destination)
+def initialize(route, destination, passengers = [])
   @route = route
   @destination = destination
+  @passengers = passengers
 end
 
 def drive()
   return ("Brum Brum")
 end
 
+def head_count()
+  return @passengers.length
 
+end
 
 end
--- specs/bus_lab_spec.rb
+++ specs/bus_lab_spec.rb
@@ -1,18 +1,26 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../bus_lab.rb")
+require_relative("../person_lab.rb")
 
 class Bustest < MiniTest::Test
 
 def setup
-  @No14 = Bus.new(14, "The Zoo")
+  passenger1 = Person.new("Tony", 30)
+  passenger2 = Person.new("Bert", 10)
+  @No14 = Bus.new(14, "The Zoo", [passenger1, passenger2])
 end
 
 def test_drive_method()
 assert_equal("Brum Brum", @No14.drive)
 end
 
+def test_head_count()
+  # assert_equal(2, @No14.passengers.length)
+  actual = @No14.head_count
+  expected = 2
+  assert_equal(expected, actual)
 
-
+end
 
 end
